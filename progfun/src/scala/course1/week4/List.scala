6925c9abfa7d0428ba4f6f56fa041bcbd4a24994
Jerry Wang <j3rry.wan9@gmail.com>
2017-08-16 23:17:21 -0700

    update progfun/src/scala/course1/week4/List.scala

--- progfun/src/scala/course1/week4/List.scala
+++ progfun/src/scala/course1/week4/List.scala
@@ -2,17 +2,18 @@ package course1.week4
 
 import java.util.NoSuchElementException
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+  def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
@@ -20,7 +21,9 @@ class Nil[T] extends List[T] {
 
 object List {
   // List(1, 2) = List.apply(1, 2)
-  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
   // List() = List.apply()
-  def apply[T]() = new Nil
+  def apply[T]() = Nil
+  // List must be covariant
+  val x: List[String] = Nil
 }
