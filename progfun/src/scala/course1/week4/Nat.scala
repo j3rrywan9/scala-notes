83fa44242382da9a83d86f1e49d71958c69906c9
Jerry Wang <j3rry.wan9@gmail.com>
2017-11-23 12:04:24 -0800

    update course1.week4.Nat.scala

--- progfun/src/scala/course1/week4/Nat.scala
+++ progfun/src/scala/course1/week4/Nat.scala
@@ -1,14 +1,45 @@
 package course1.week4
 
-// Provide an implementation of the abstract class Nat that represents non-negative integers.
+/**
+  * Provide an implementation of the abstract class Nat that represents non-negative integers.
+  */
 abstract class Nat {
+  /**
+    * Test whether a given natural number is zero.
+    * @return
+    */
   def isZero: Boolean
+
+  /**
+    * Return the natural number before a positive natural number.
+    * @return
+    */
   def predecessor: Nat
+
+  /**
+    * Return the next natural number after the current natural number.
+    * @return
+    */
   def successor: Nat = new Succ(this)
+
+  /**
+    * Addition.
+    * @param that
+    * @return
+    */
   def +(that: Nat): Nat
+
+  /**
+    * Subtraction.
+    * @param that
+    * @return
+    */
   def -(that: Nat): Nat
 }
 
+/**
+  * The object represents number zero.
+  */
 object Zero extends Nat {
   override def isZero: Boolean = true
 
