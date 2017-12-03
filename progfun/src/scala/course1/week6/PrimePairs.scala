8baa4d3fcbaded7fa8461cb99edb2f63327a740f
Jerry Wang <j3rry.wan9@gmail.com>
2017-12-02 22:17:26 -0800

    update course1.week6.PrimePairs.scala

--- progfun/src/scala/course1/week6/PrimePairs.scala
+++ progfun/src/scala/course1/week6/PrimePairs.scala
@@ -1,29 +1,49 @@
-object pairs {
+package course1.week6
+
+/**
+  * Given a positive integer n, find all pairs of positive integers i and j, with 1 <= j < i < n such that i + j is prime.
+  */
+object PrimePairs extends App {
+  /**
+    * Check if an integer is a prime.
+    * @param n the integer.
+    * @return
+    */
   def isPrime(n: Int) = (2 until n) forall (n % _ != 0)
 
   val n = 7
 
-  // sequence of sequences
-  (1 until n) map (i =>
+  val xss = (1 until n) map (i =>
     (1 until i) map (j => (i, j)))
+  println(xss)
 
-  ((1 until n) map (i =>
-    (1 until i) map (j => (i, j)))).flatten
+  /**
+    * The previous step gave a sequence of sequences.
+    * We can combine all the sub-sequences using foldRight with ++.
+    */
+  println((xss foldRight Seq[(Int, Int)]())(_ ++ _))
 
-  (1 until n) flatMap (i =>
+  /**
+    * Or, equivalently, we use the built-in method flatten.
+    */
+  println(xss.flatten)
+
+  /**
+    * xs flatMap f = (xs map f).flatten
+    */
+  val yss = (1 until n) flatMap (i =>
     (1 until i) map (j => (i, j)))
+  println(yss)
 
-  (1 until n) flatMap (i =>
-    (1 until i) map (j => (i, j))) filter (pair => isPrime(pair._1 + pair._2))
+  println(yss filter (pair => isPrime(pair._1 + pair._2)))
 
-  for {
+  /**
+    * Use for expression.
+    */
+  val zss = for {
     i <- 1 until n
     j <- 1 until i
     if isPrime(i + j)
   } yield (i, j)
-
-  def scalarProduct(xs: List[Double], ys: List[Double]): Double =
-    (for ((x, y) <- xs zip ys) yield x * y).sum
-
-  scalarProduct(List(1, 2, 3), List(1, 2, 3))
+  println(zss)
 }
